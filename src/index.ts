43b48a9ce801cd3de163880863cc5b4373571e93
Thawane <thawaneapoliveira@gmail.com>
2023-03-24 18:09:29 -0300

    feat: adiciona requisito 13, implementado index na raiz de src para criar players, monsters, batalhas etc

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import Character from './Character';
+import Monster from './Monster';
+import Dragon from './Dragon';
+import Battle, { PVP, PVE } from './Battle';
+
+// 3 objetos do tipo Character
+
+const player1 = new Character('player1');
+const player2 = new Character('player2');
+const player3 = new Character('player3');
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+// fly stone free
+
+// 2 objetos do tipo Monster
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+// 1 objeto do tipo PVP
+
+const pvp = new PVP(player2, player3);
+
+// 1 objeto do tipo PVE
+
+const pve = new PVE(player1, [monster1, monster2]);
+
+// função runBattles
+
+const runBattles = (battles: Battle[]) => {
+  battles.map((battle) => battle.fight());
+};
+
+export {
+  player1, player2, player3,
+  monster1, monster2,
+  pvp, pve,
+  runBattles,
+};
